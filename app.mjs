5bb5d51713d7d2cac557628c27802ba919b66bc0
Areebasagheer <158015118+Areebasagheer@users.noreply.github.com>
2025-04-26 07:09:21 +0500

    first-server

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    console.log(req.ip)
+  res.send('Hello World!👋\nThis is my first server')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// app.get('/Weather:/cityName/:side', (req,res) => {
+//   res.send(req.params)
+// })
